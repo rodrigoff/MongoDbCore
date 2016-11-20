7cf464efbb1cf8381521c8db007930cf5cd2bde4
Rodrigo F. Fernandes <rodrigoff@live.com>
2016-11-20 10:33:27 -0200

    Added Autofac to cross cutting concerns

---
+++ src/MongoDbCore.CrossCutting/DependencyInjection/DefaultModule.cs
@@ -0,0 +1,12 @@
+﻿using Autofac;
+
+namespace MongoDbCore.CrossCutting.DependencyInjection
+{
+    public class DefaultModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            base.Load(builder);
+        }
+    }
+}
