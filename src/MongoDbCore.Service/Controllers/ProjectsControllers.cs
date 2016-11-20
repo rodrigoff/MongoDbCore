34035ef033b29593e7bd2045879aa6da8211cbb8
Rodrigo F. Fernandes <rodrigoff@live.com>
2016-11-20 15:42:27 -0200

    Added skeleton for ProjectsController

---
+++ src/MongoDbCore.Service/Controllers/ProjectsControllers.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDbCore.Domain;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MongoDbCore.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProjectsControllers : Controller
+    {
+        [HttpGet]
+        public async Task<IEnumerable<Project>> Get()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<Project> Get(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task Post([FromBody] Project project)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [HttpPut("{id}")]
+        public async Task Put(int id, [FromBody] Project project)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
