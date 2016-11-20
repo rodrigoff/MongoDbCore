7917fd886032b5da656bf69d1b98b06ba5998a5d
Rodrigo F. Fernandes <rodrigoff@live.com>
2016-11-20 19:03:06 -0200

    Added Team and TeamMember entities

---
+++ src/MongoDbCore.Domain/Team.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System.Collections.Generic;
+
+namespace MongoDbCore.Domain
+{
+    public class Team : BaseEntity
+    {
+        [BsonConstructor]
+        public Team(string name)
+        {
+            Name = name;
+        }
+
+        [BsonElement(nameof(Name))]
+        public string Name { get; set; }
+
+        public ICollection<MongoDBRef> TeamMembers { get; set; }
+    }
+}
---
+++ src/MongoDbCore.Domain/TeamMember.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System.Collections.Generic;
+
+namespace MongoDbCore.Domain
+{
+    public class TeamMember : BaseEntity
+    {
+        [BsonConstructor]
+        public TeamMember(string name)
+        {
+            Name = name;
+        }
+
+        [BsonElement(nameof(Name))]
+        public string Name { get; set; }
+
+        public ICollection<MongoDBRef> Teams { get; set; }
+    }
+}
