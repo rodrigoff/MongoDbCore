1fec835f4d17ead8128669a3ba6c58348a43b6e1
Rodrigo F. Fernandes <rodrigoff@live.com>
2016-11-20 15:42:15 -0200

    Added skeleton for UsersController

---
+++ src/MongoDbCore.Service/Controllers/UsersController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDbCore.Domain;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MongoDbCore.Controllers
+{
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        public async Task<IEnumerable<User>> Get()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<User> Get(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [HttpPost]
+        public async Task Post([FromBody] User user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [HttpPut("{id}")]
+        public async Task Put(int id, [FromBody] User user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
